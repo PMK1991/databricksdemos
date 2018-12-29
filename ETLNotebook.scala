224dee724685683afe8452b00664ae1a0d6e4d08
PMK1991 <37069933+PMK1991@users.noreply.github.com>
2018-12-29 23:44:15 +0530

    Add files via upload

--- ETLNotebook.scala
+++ ETLNotebook.scala
@@ -1,7 +1,7 @@
 // Databricks notebook source
 spark.conf.set("dfs.adls.oauth2.access.token.provider.type", "ClientCredential")
-spark.conf.set("dfs.adls.oauth2.client.id", "26f0200e-0880-4d27-9691-0e6323c267bf")
-spark.conf.set("dfs.adls.oauth2.credential", "FRzYOFb46pZh8Jf5+hWMw3l1pTJybf6kcQ/e197MTTo=")
+spark.conf.set("dfs.adls.oauth2.client.id", "817e80ee-4aae-41fb-b88d-ca588ed60e4f")
+spark.conf.set("dfs.adls.oauth2.credential", "wmts8PmtqHUbyWJMEuQOAsWANFWpcIfMOI1UDOQ7ZZs=")
 spark.conf.set("dfs.adls.oauth2.refresh.url", "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
 
 // COMMAND ----------
@@ -9,12 +9,12 @@ spark.conf.set("dfs.adls.oauth2.refresh.url", "https://login.microsoftonline.com
 //spark.conf.set("dfs.adls.oauth2.access.token.provider.type", "ClientCredential")
 //spark.conf.set("dfs.adls.oauth2.client.id", "<Application ID>")
 //spark.conf.set("dfs.adls.oauth2.credential", "<Key>")
-//spark.conf.set("dfs.adls.oauth2.refresh.url", "<https://login.microsoftonline.com/<Directory ID>>")
+//spark.conf.set("dfs.adls.oauth2.refresh.url", "<https://login.microsoftonline.com/<Directory ID>/oauth2/token>")
 
 // COMMAND ----------
 
-val jdbcUsername = dbutils.secrets.get(scope = "key-vault-secrets", key = "username")
-val jdbcPassword = dbutils.secrets.get(scope = "key-vault-secrets", key = "password")
+val jdbcUsername = dbutils.secrets.get(scope = "AvroScope", key = "username")
+val jdbcPassword = dbutils.secrets.get(scope = "AvroScope", key = "password")
 
 // COMMAND ----------
 
@@ -34,7 +34,11 @@ connectionProperties.put("password", s"${jdbcPassword}")
 
 // COMMAND ----------
 
-val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("adl://srcdatalake.azuredatalakestore.net/FinancialData/cs-training_new.csv")
+val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("adl://srcdl.azuredatalakestore.net/FinancialData/cs-training_new.csv")
+
+// COMMAND ----------
+
+val df = spark.read.csv("adl://srcdl.azuredatalakestore.net/FinancialData/cs-training_new.csv")
 
 // COMMAND ----------
 
